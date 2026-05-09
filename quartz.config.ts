cae2a23c21dcf2aa6520459b89fa0aa1d6de63ad
Mustafa-Dara-Ozevin <mustafadara.ozevin@gmail.com>
2026-05-09 14:20:09 +0300

    Quartz sync: May 9, 2026, 2:20 PM

--- quartz.config.ts
+++ quartz.config.ts
@@ -16,7 +16,7 @@ const config: QuartzConfig = {
       provider: "plausible",
     },
     locale: "en-US",
-    baseUrl: "quartz.jzhao.xyz",
+    baseUrl: "mustafa-dara-ozevin.github.io/American_Studies",
     ignorePatterns: ["private", "templates", ".obsidian"],
     defaultDateType: "modified",
     theme: {
